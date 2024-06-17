d9aa530134f61cc5865ea48fddb4f93d73873f7b
BastianSaavedra <bastiansaavedra55@gmail.com>
2024-06-17 13:06:08 -0400

    modified: rule.mk in qmk config | adding .hex

--- qmk_none/rules.mk
+++ qmk_none/rules.mk
@@ -3,6 +3,7 @@ RGBLIGHT_ENABLE		= no
 RGB_MATRIX_ENABLE	= yes    # Enable WS2812 RGB underlight.
 VIA_ENABLE			= yes    # Enable VIA
 OLED_ENABLE			= yes
+OLED_DRIVER_ENABLE  = yes
 LTO_ENABLE			= yes
 WPM_ENABLE			= yes
 TAP_DANCE_ENABLE	= yes
