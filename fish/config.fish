35e602a7d32130e561aa9246487c976d97966d9d
BastianSaavedra <bastiansaavedra55@gmail.com>
2024-07-20 01:34:02 -0400

    setting cmp completion

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,36 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-  alias sscrot="scrot ~/Pictures/Screenshots/%b%d::%H%M%S.png"
-  set -gx PATH /usr/local/bin $PATH
-  alias pyvenv="python3 -m venv"
-  alias pyvenvcloud="python3 -m virtualenv --always-copy venv"
-  alias activate="source venv/bin/activate.fish"
-  alias token="cat ~/pCloudDrive/Workspace/Utilities/Tokens/git_token.txt | xclip -selection clipboard"
-  alias eqtile="nvim ~/.dotfiles/qtile/"
-  alias envim="nvim ~/.dotfiles/nvim/"
-  alias ealacritty="nvim ~/.dotfiles/alacritty/"
-  alias erofi="nvim ~/.dotfiles/rofi/"
-  alias efish="nvim ~/.dotfiles/fish/"
-  alias eomf="nvim ~/.dotfiles/omf/"
-  alias eranger="nvim ~/.dotfiles/ranger/"
-  alias ekitty="nvim ~/.dotfiles/kitty/"
-  alias eneofetch="nvim ~/.dotfiles/neofetch/"
+    alias sscrot="scrot ~/Pictures/Screenshots/%b%d::%H%M%S.png"
+    set -gx PATH /usr/local/bin $PATH
+    # python alias
+    alias pyvenv="python3 -m venv"
+    alias pyvenvcloud="python3 -m virtualenv --always-copy venv"
+    alias activate="source venv/bin/activate.fish"
+    alias pyright="cp ~/pCloudDrive/Workspace/Utilities/Python/pyrightconfig.json ."
+
+    alias mongodbstart="sudo systemctl start mongodb.service"
+    alias mongodbstop="sudo systemctl stop mongodb.service"
+
+    alias postgrestart="sudo systemctl start postgresql.service"
+    alias postgrestop="sudo systemctl stop postgresql.service"
+
+    # git token alias
+    alias token="cat ~/pCloudDrive/Workspace/Utilities/Tokens/git_token.txt | xclip -selection clipboard"
+
+    # edit alias
+    alias eqtile="nvim ~/.dotfiles/qtile/"
+    alias envim="nvim ~/.dotfiles/nvim/"
+    alias ealacritty="nvim ~/.dotfiles/alacritty/"
+    alias erofi="nvim ~/.dotfiles/rofi/"
+    alias efish="nvim ~/.dotfiles/fish/"
+    alias eomf="nvim ~/.dotfiles/omf/"
+    alias eranger="nvim ~/.dotfiles/ranger/"
+    alias ekitty="nvim ~/.dotfiles/kitty/"
+    alias eneofetch="nvim ~/.dotfiles/neofetch/"
 
 
 end
+
+# bun
+set --export BUN_INSTALL "$HOME/.local/share/reflex/bun"
+set --export PATH $BUN_INSTALL/bin $PATH
